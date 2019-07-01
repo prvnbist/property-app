20562dd6a43915828d6d2c35ef7e8cca5c0e93f5
Praveen Bisht <prvnbist@gmail.com>
2019-07-01 17:28:07 +0530

    Added: User Authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://admin:admin123@ds245647.mlab.com:45647/property-app" 
-  };
+    mongoURI:
+        'mongodb://admin:admin123@ds245647.mlab.com:45647/property-app',
+    key: 'secret',
+};
--- server.js
+++ server.js
@@ -1,35 +1,51 @@
 const express = require('express');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Bodyparser middleware
 app.use(
     bodyParser.urlencoded({
-      extended: false
+        extended: false,
+    }),
+);
+app.use(bodyParser.json());
+// DB Config
+const db = require('./config/keys').mongoURI;
+// Connect to MongoDB
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
     })
-  );
-  app.use(bodyParser.json());
-  // DB Config
-  const db = require("./config/keys").mongoURI;
-  // Connect to MongoDB
-  mongoose
-    .connect(
-      db,
-      { useNewUrlParser: true }
-    )
-    .then(() => console.log("MongoDB successfully connected"))
+    .then(() => console.log('MongoDB successfully connected'))
     .catch(err => console.log(err));
 
-app.get('/yo', (req,res) => res.send({yo: "Me"}));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
+
+app.get('/yo', (req, res) =>
+    res.send({
+        yo: 'Me',
+    }),
+);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`),
+);
