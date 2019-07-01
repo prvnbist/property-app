00e4c7b732c0c6d51c8e4370515cbc7d7f9a825a
Praveen Bisht <prvnbist@gmail.com>
2019-07-01 16:43:17 +0530

    Connected: DB

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require('path');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/data', (req, res) => res.send({data: 'Praveen Bisht'}))
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
+  // DB Config
+  const db = require("./config/keys").mongoURI;
+  // Connect to MongoDB
+  mongoose
+    .connect(
+      db,
+      { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+app.get('/yo', (req,res) => res.send({yo: "Me"}));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
